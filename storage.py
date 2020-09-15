4263b7f7c0dec02973b1fa7e3e6e6b438d24450c
alenatorgasheva <in-fa@mail.ru>
2020-09-15 11:26:44 +0700

    commit 1.

---
+++ storage.py
@@ -0,0 +1,62 @@
+import argparse
+import os
+import tempfile
+import json
+
+
+# Functions:
+def reading(storage_path):
+    """Reading data"""
+    with open(storage_path, 'r') as f:
+        data = json.load(f)
+    return data
+
+
+def writing(storage_path, data):
+    """Writing data"""
+    with open(storage_path, 'w') as f:
+        json.dump(data, f)
+
+
+def create_arguments():
+    """Creating arguments"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--key", help='Key word', required=True)
+    parser.add_argument("--val", help='Value of key word', required=False)
+    args = parser.parse_args()
+    return args
+
+
+def key(args, data):
+    """Printing data by key word (--key)"""
+    if args.key in data:
+        print(', '.join(data[args.key]))
+    else:
+        print(None)
+
+
+def val(args, data):
+    """Adding data (--val)"""
+    if args.key in data:
+        data[args.key].append(args.val)
+    else:
+        data[args.key] = [args.val]
+
+
+# Main:
+def main():
+
+    storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
+    data = reading(storage_path)
+    args = create_arguments()
+
+    # Doing actions:
+    if not args.val:
+        key(args, data)
+    elif args.val:
+        val(args, data)
+
+    writing(storage_path, data)
+
+
+main()
